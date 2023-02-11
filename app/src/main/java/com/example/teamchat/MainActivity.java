f8444f2c6fa686c25bf94ec3611b13a0fbe7d0dd
liger2020 <60995937+liger2020@users.noreply.github.com>
2023-02-11 13:49:31 +0800

    Made changes to the Activity_main.xml layout. Added Camera, Storage and Audio Permission.

--- app/src/main/java/com/example/teamchat/MainActivity.java
+++ app/src/main/java/com/example/teamchat/MainActivity.java
@@ -41,6 +41,8 @@ public class MainActivity extends AppCompatActivity {
                 Manifest.permission.ACCESS_WIFI_STATE,
                 Manifest.permission.ACCESS_COARSE_LOCATION,
                 Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.CAMERA,
+                Manifest.permission.RECORD_AUDIO
         };
 
         if (!hasPermissions(this, PERMISSIONS)) {
